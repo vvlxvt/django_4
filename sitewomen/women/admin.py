d3869a17bd357b18afd68673c74537fa798ee60f
vvlxvt <vvlx@tutanota.com>
2024-02-29 14:40:05 +0400

    Урок 39. В админку добавлено новое поле которое считает кол-во символов статьи, так сортируется

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -3,15 +3,20 @@ from .models import Women, Category
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     # отображаем поля, которые будут видны в админке
-    list_display = ('id','title','time_create','cat','is_published')
+    list_display = ('title','time_create','cat','is_published', 'brief_info')
     # указаваем поля кт будут кликабельны в админке
-    list_display_links = ('id','title')
+    list_display_links = ('title',)
     # указаваем последовательность сортировки записей
     ordering = ('time_create','title')
     # указаваем поля кт можно изменить, при этом они не должны тогда быть кликабельными
     list_editable = ('is_published',)
     # пагинация списка записей (сколько показывать на странице)
     list_per_page = 4
+
+    @admin.display(description='Краткое описание', ordering= 'content')
+    def brief_info(self, women: Women):
+        return f'Описание {len(women.content)} символов'
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
