27fa83b251c38166ee1ebf786e592ae28e6ce7ad
vvlxvt <vvlx@tutanota.com>
2024-03-02 09:11:11 +0400

    Урок 40. В админку добавлены панель поиска и фильтры, в том числе собственый фильтр

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,5 +1,19 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Women, Category
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [('married', 'замужем'),
+        ('single', 'не замужем'),]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     # отображаем поля, которые будут видны в админке
@@ -12,10 +26,27 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published',)
     # пагинация списка записей (сколько показывать на странице)
     list_per_page = 4
+    # добавляем новое действие
+    actions = ['set_published', 'set_draft']
+    search_fields = ('title','cat__name',)
+    list_filter = (MarriedFilter,'cat__name', 'is_published')
 
     @admin.display(description='Краткое описание', ordering= 'content')
+    # добавляем дополнительное поле к записям в админку
     def brief_info(self, women: Women):
         return f'Описание {len(women.content)} символов'
+
+    @admin.action(description='Опубликовать выбранные записи')
+    # добавляем действие к выбранным записям в админку
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published = Women.Status.PUBLISHED)
+        self.message_user(request, f'Изменено {count} записей')    \
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published = Women.Status.DRAFT)
+        self.message_user(request, f'{count} записей снято с публикации', messages.WARNING)
+
 
 
 @admin.register(Category)
