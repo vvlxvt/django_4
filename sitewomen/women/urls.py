7d5be51043344ada71bc0b88954e4e4e73596d41
vvlxvt <vvlx@tutanota.com>
2024-03-13 14:32:12 +0400

    Урок 52. прописал класс представления WomenCategory с наследованием от встроенного класса ListView

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('addpage/', views.AddPage.as_view(), name='addpage'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
-    path('category/<slug:cat_slug>/', views.show_category, name='category'),
+    path('category/<slug:cat_slug>/', views.WomenCategory.as_view(), name='category'),
     path('post/<slug:post_slug>/', views.show_post, name='post'),
     path('tag/<slug:tag_slug>/', views.show_tag_postlist, name='tag'),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,32 +4,51 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Women, Category, TagPost, UploadFiles
 
+
+
 menu = [
     {'title': 'О сайте', 'url_name': "about"},
     {'title': "Добавить статью", 'url_name': "addpage"},
-    {'title': "Обратная связь", 'url_name':'contact'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
     {'title': "Войти", 'url_name': 'login'}
 ]
 
-class WomenHome(TemplateView):
+# def index(request):
+#     posts = Women.published.all().select_related('cat')
+#     data = {
+#         'title': 'Главная страница',
+#         'menu': menu,
+#         'posts': posts,
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'women/index.html', context=data)
+
+class WomenHome(ListView):
+    # предполагает получыение данных из таблицы
     template_name = 'women/index.html'
+    model = Women
+    context_object_name = 'posts'
+    # определяем переменную  кт будет содержать список статей
     extra_context = {
         # этот словарь для статических данных
         'title': 'главная страница',
         'menu': menu,
-        'posts': Women.published.all().select_related('cat'), # получение всех записей из модели Women
         'cat_selected': 0,
         }
+
+    def get_queryset(self):
+        return Women.published.all().select_related('cat')
+
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
-    #     context['title'] = 'главная страница',
-    #     context['menu'] = menu,
-    #     context['posts'] = Women.published.all().select_related('cat'),  # получение всех записей из модели Women
+    #     context['title'] = 'главная страница'
+    #     context['menu'] = menu
+    #     context['posts'] = Women.published.all().select_related('cat')  # получение всех записей из модели Women
     #     context['cat_selected'] = int(self.request.GET.get('cat_id',0))
     #     return context
 
@@ -54,15 +73,34 @@ def show_post(request, post_slug):
         }
     return render(request, 'women/post.html', data)
 
-def show_category(request, cat_slug):
-    category = get_object_or_404(Category, slug = cat_slug)
-    posts = Women.published.filter(cat_id = category.pk).select_related('cat')
-    data = {'title': f'Рубрика: {category.name}',
-            'menu': menu,
-            'posts': posts,
-            'cat_selected': category.pk,
-            }
-    return render(request, 'women/index.html', context = data)
+# def show_category(request, cat_slug):
+#     category = get_object_or_404(Category, slug = cat_slug)
+#     posts = Women.published.filter(cat_id = category.pk).select_related('cat')
+#     data = {'title': f'Рубрика: {category.name}',
+#             'menu': menu,
+#             'posts': posts,
+#             'cat_selected': category.pk,
+#             }
+#     return render(request, 'women/index.html', context = data)
+
+class WomenCategory(ListView):
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False # позволить пустой список категорий (отображается ошибка 404)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = context['posts'][0].cat
+        print(cat)
+        context['title'] = 'Категория - ' + cat.name
+        context['menu'] = menu
+        context['cat_selected'] = cat.pk
+        return context
+
+    def get_queryset(self):
+        return Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
+
+
 
 def show_tag_postlist(request, tag_slug):
     # прочитаем пост из модели TagPost по слагу tag_slug
