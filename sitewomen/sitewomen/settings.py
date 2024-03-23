2c3b50efb4d361818d0cd1216554207ab33a3dce
vvlxvt <vvlx@tutanota.com>
2024-03-23 13:36:08 +0400

    Урок 59. добавил приложение users для аутенфикации: добавил 'users' в settings.py, прописал путь в sitewomen/urls.py, добавил 2 функции представления login/logout

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'women.apps.WomenConfig',
+    'users',
     'debug_toolbar',
 ]
 
--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -24,6 +24,7 @@ from women.views import page_not_found
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
