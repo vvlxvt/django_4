182561e0c1d61eb892901d967799bc10c9242b17
vvlxvt <vvlx@tutanota.com>
2024-03-27 19:42:15 +0400

    Урок 64 добавил класс формы RegisterUserForm для регистрации пользователей, прописал путь к шаблону

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -7,4 +7,5 @@ appname = 'users'
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
