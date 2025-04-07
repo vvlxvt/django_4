69107767aab807b4b183a3f10f1c10da16689d60
vvlxvt <vvlx@tutanota.com>
2025-04-07 18:46:41 +0400

    created handmade functions for encode model and decode model

--- sitewomen/women/serializers.py
+++ sitewomen/women/serializers.py
@@ -1,8 +1,31 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title','cat_id')
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+# def encode():
+#     model = WomenModel('Sher', 'Content: Sher')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Sher","content":"Content: Sher"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
