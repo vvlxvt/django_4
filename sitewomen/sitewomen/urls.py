eff5a75d6b7ec6eaf5efeb0ea33623ce7ba1bd3b
vvlxvt <vvlx@tutanota.com>
2025-04-13 13:21:34 +0400

    added Session-based authentication

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/v1/', include(router.urls)),
     path('', include('women.urls')),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/women', WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>', WomenAPIDestroy.as_view()),
